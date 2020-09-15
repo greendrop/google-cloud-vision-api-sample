64ea888ebbc51c03e918cccae0b60da13e0fd3d9
greendrop <greendrop.devel@gmail.com>
2020-09-16 00:30:52 +0900

    modify layout and unused files

---
+++ src/components/organisms/DrawerContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon small>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+@Component
+export default class DrawerContent extends Vue {
+  @Prop({ type: Array, required: true })
+  items!: typeof Object[]
+}
+</script>
